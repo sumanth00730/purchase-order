427e6e3888d886f5f575f04d78e28216d9535d3b
sumanth <sumanthmahilamuri007@gmail.com>
2022-05-24 17:52:21 +0530

    uuid and params added

---
+++ db/migrate/20220524094724_create_purchase_orders.rb
@@ -0,0 +1,16 @@
+class CreatePurchaseOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :purchase_orders, id: :uuid do |t|
+      t.date :delivery_date
+      t.date :date
+      t.text :payment_terms
+      t.float :amount
+      t.float :cgst
+      t.float :sgst
+      t.float :igst
+      t.float :tds
+      t.text :description
+      t.timestamps
+    end
+  end
+end
